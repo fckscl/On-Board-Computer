e3dda89704c209ddbb74714e46211156147eecf0
fckscl <dimapugach31102003@gmail.com>
2023-02-24 01:03:42 +0300

    refactor ui

--- app/src/main/java/com/example/on_boardcomputer/ui/repair/RepairsViewModel.kt
+++ app/src/main/java/com/example/on_boardcomputer/ui/repair/RepairsViewModel.kt
@@ -9,36 +9,8 @@ class RepairsViewModel(
     val dataSource: RepairDatabaseDao,
     application: Application
 ) : AndroidViewModel(application) {
-    private var viewModelJob = Job()
-    private val uiScope = CoroutineScope(Dispatchers.Main +  viewModelJob)
 
     var sum = dataSource.sum()
 
-//    private suspend fun getSum(): Double {
-//        return withContext(Dispatchers.IO) {
-//            var measurement = dataSource.sum()
-//
-//            if (measurement == null) {
-//                measurement = 0.0
-//            }
-//
-//            measurement
-//        }
-//    }
-
     val measurements = dataSource.getAll()
-//    fun getSumSync(){
-//        uiScope.launch {
-//            sum = getSum()
-//        }
-//    }
-//
-//    override fun onCleared() {
-//        super.onCleared()
-//        viewModelJob.cancel()
-//    }
-//    init {
-////        getSumSync()
-//        sum = 0.0
-//    }
 }
